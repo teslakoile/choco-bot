b923aa9136cf3c103d26633f56f818df881fa90c
Kyle Nathan Naranjo <63973445+teslakoile@users.noreply.github.com>
2024-02-01 22:56:21 +0800

    feat(initial commit): initial commit

---
+++ bot.py
@@ -0,0 +1,31 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+
+intents.messages = True
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+load_dotenv()
+token = os.getenv("DISCORD_BOT_TOKEN")
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if client.user in message.mentions:
+        await message.reply("Hello! I am Choco!")
+
+
+client.run(token)
